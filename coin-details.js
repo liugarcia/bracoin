53e95374e9fa09ddc2d8260d67b3b136dfb8694e
liugarcia <118115099+liugarcia@users.noreply.github.com>
2025-07-05 19:59:07 -0300

    Update coin-details.js

--- coin-details.js
+++ coin-details.js
@@ -91,9 +91,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 },
                 // AJUSTE CRÍTICO AQUI: padding para o gráfico
                 padding: {
-                    top: 2,   // Diminua este valor para mover o gráfico mais para cima
+                    top: 5,   // Diminua este valor para mover o gráfico mais para cima
                     right: 0,
-                    bottom: 10, // Diminua ou ajuste conforme a necessidade para a parte inferior
+                    bottom: 12, // Diminua ou ajuste conforme a necessidade para a parte inferior
                     left: 0
                 }
             },
